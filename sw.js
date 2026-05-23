bee86c21c5063c2ae98ec865edaacb8a7a1db825
ShenQi-lab <73091130+ShenQi-lab@users.noreply.github.com>
2026-05-23 23:51:36 +0800

    修改管理菜单样式；新增Service Worker离线缓存模式

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'food-spinner-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './icon-192.png',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
